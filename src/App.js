77270f66ddf4922f69ba259d31c6be6ef66a41f6
Victor Sirotilo <75179111+vVictoRras@users.noreply.github.com>
2022-10-29 20:18:46 +0300

    load
    load1

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+import '../src/components/inc/stars.scss'
+// import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Intro from './components/inc/Intro';
+import Contact from './components/pages/Contact';
+import Navbar from './components/inc/Navbar';
+import Footer from './components/inc/Footer';
+import Skills from './components/pages/Skills';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import { GlobalStyles } from './components/inc/globalStyles';
+
+//import { lightTheme, darkTheme } from './components/ThemeSwitch';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+        <Route  exact path="/">
+          <Intro />
+          {/*<Home/>*/}
+        </Route>
+
+
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/contacts">
+          <Contact />
+        </Route>
+
+        <Route path="/skills">
+          <Skills />
+        </Route>
+
+        </Switch>
+        <Footer />
+      </div>
+
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/inc/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+import {
+  BrowserRouter as Router
+
+} from "react-router-dom";
+import { Navbar,Nav } from 'react-bootstrap'
+
+class BootNavbar extends React.Component{
+
+  render(){
+    return(
+        <div>
+          <div className="row">
+            <div className="col-md-12">
+              <Router>
+                <Navbar bg="light" variant="light" expand="lg" sticky="top">
+                  <Navbar.Brand href="/">My Portfolio</Navbar.Brand>
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                      {/*<Nav.Link href="/">Home</Nav.Link>*/}
+                      <Nav.Link href="/skills">Skills</Nav.Link>
+                      <Nav.Link href="/about">About</Nav.Link>
+                      <Nav.Link href="/contacts">Contacts</Nav.Link>
+                      </Nav>
+
+                  </Navbar.Collapse>
+
+                </Navbar>
+
+              </Router>
+            </div>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default BootNavbar;
